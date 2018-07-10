5cb267fbbde0437e128cc552c6a890af67783341
HighCWu <HighCWu@163.com>
2018-07-10 10:53:54 +0800

    task1 finished by HighCWu

---
+++ task1/HighCWu/task1/console.cpp
@@ -0,0 +1,28 @@
+﻿#include "console.h"
+#include <QTextCursor>
+#include <QTextBlock>
+
+/**
+ * @name log
+ * @namespace Console
+ * @brief 用于打印日志到控制台
+ * @param  text 欲打印到控制台的内容
+ * @param  flush 是否更新同一行的输出内容
+ * @return void
+ */
+void Console::log(const QString &text, const bool &flush)
+{
+    QTextCursor cursor=m_consoleBox->textCursor();
+    if(flush && flush_count > 0) {
+        int position = m_consoleBox->document()->findBlockByNumber(m_consoleBox->document()->blockCount()-2).position();
+        cursor.setPosition(position);
+        cursor.select(QTextCursor::BlockUnderCursor);
+        cursor.removeSelectedText();
+    }
+    if(flush) flush_count++;
+    else flush_count = 0;
+    m_consoleBox->setText(m_consoleBox->toPlainText()+text+'\n');
+    //保持编辑器在光标最后一行
+    cursor.movePosition(QTextCursor::End);
+    m_consoleBox->setTextCursor(cursor);
+}
